- Add unit tests for MetricSpace (and all subclasses) - Fix bugs in Sphere implementation - Fix indexing bugs - Add n_jobs parameter to *_medoid functions for computation of the distances

pyfrechet/metric_spaces/metric_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Union, Any, TypeVar
-from .utils import D_mat_par, mat_sel_idx, mat_sel, coalesce_weights
+import numbers
+from .utils import D_mat_par, D_mat, mat_sel_idx, mat_sel, coalesce_weights
 
 T = TypeVar("T", bound="MetricData")
 
@@ -13,10 +14,10 @@ class MetricData:
 
     def compute_distances(self, n_jobs=-2):
         if self.distances is None:
-            self.distances = D_mat_par(self.M, self.data, n_jobs)
-
-    def d(self, i, j):
-        return self.distances[i,j] if self.distances else self.M.d(self[i], self[j])
+            if n_jobs is None or n_jobs == 1:
+                self.distances = D_mat(self.M, self.data)
+            else:
+                self.distances = D_mat_par(self.M, self.data, n_jobs)
 
     def frechet_mean(self, weights=None):
         return self.M.frechet_mean(self.data, weights)
@@ -24,24 +25,25 @@ class MetricData:
     def frechet_var(self, weights=None):
         return self.M.frechet_var(self.data, weights)
     
-    def frechet_medoid(self, weights=None):
-        self.compute_distances()
+    def frechet_medoid(self, weights=None, n_jobs=-2):
+        self.compute_distances(n_jobs=n_jobs)
         weights = coalesce_weights(weights, self)
         idx = np.argmin(self.distances.dot(weights))
         return self[idx]
 
-    def frechet_medoid_var(self, weights=None):
-        self.compute_distances()
+    def frechet_medoid_var(self, weights=None, n_jobs=-2):
+        self.compute_distances(n_jobs=n_jobs)
         weights = coalesce_weights(weights, self)
         return np.min(self.distances.dot(weights))
 
     def __getitem__(self, key) -> Union[Any, T]:
         subset = self.M.index(self.data, key)
-        if type(key) is int:
+        if isinstance(key, numbers.Integral):
             return subset
         elif self.distances is None:
             return MetricData(self.M, subset)
         else:
+            key = key if type(key) is np.ndarray else np.array(key)
             subdist = mat_sel(self.distances, key) if key.dtype == 'bool' else mat_sel_idx(self.distances, key)
             return MetricData(self.M, subset, subdist)
         

pyfrechet/metric_spaces/sphere.py
@@ -10,7 +10,13 @@ class Sphere(MetricSpace):
         self.dim = dim
 
     def _d(self, x, y):
-        return np.sqrt(np.sum(np.square(np.arccos(np.dot(x,y.T))), axis=0))
+        x_d_y = np.dot(x,y.T)
+        if x_d_y > 1.0 - np.finfo(x.dtype).eps:
+            return 0.0
+        elif x_d_y < -1.0 + np.finfo(x.dtype).eps:
+            return np.pi
+        
+        return np.sqrt(np.sum(np.square(np.arccos(x_d_y)), axis=0))
     
     def _frechet_mean(self, y, w):
         manifold = pymanopt.manifolds.Sphere(self.dim)
@@ -18,13 +24,15 @@ class Sphere(MetricSpace):
         def _d(x, y): return anp.sum(anp.square(anp.arccos(anp.dot(x,y.T))), axis=0)
         
         @pymanopt.function.autograd(manifold)
-        def cost(om): return anp.dot(w, _d(om.reshape((1,2)), y))
+        def cost(om): return anp.dot(w, _d(om.reshape((1,self.dim)), y))
 
         problem = pymanopt.Problem(manifold, cost)
         optimizer = pymanopt.optimizers.TrustRegions(verbosity=0)
 
         # initial y 
         y_init = w.dot(y)
+        # add a small perturbation to y_init to make sure we don't land in the sample
+        y_init += 1e-4 * np.random.randn(self.dim)
         y_init /= np.linalg.norm(y_init)
 
         result = optimizer.run(problem, initial_point=y_init)